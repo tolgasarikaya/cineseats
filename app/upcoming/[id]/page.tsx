8913a8194d8818c54c186d2b93f8e079f6b72fc7
Tolga SARIKAYA <tolga.sarikaya.dev@gmail.com>
2025-02-17 23:46:16 +0300

    movie detail page created

--- app/upcoming/[id]/page.tsx
+++ app/upcoming/[id]/page.tsx
@@ -1,10 +1,9 @@
 import MovieDetail from "@/components/movie-detail/movie-detail";
 
-type PageProps = {
-  params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
+interface PageProps {
+  params: { id: string; type: string };
+}
 
-export default async function MovieDetailPage({ params }: PageProps) {
+export default function MovieDetailPage({ params }: PageProps) {
   return <MovieDetail movieId={params.id} type="upcoming" />;
 }
