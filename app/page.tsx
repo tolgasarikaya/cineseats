862a765fe2091c73bf7fee5400cdccb98b3f28e0
Tolga SARIKAYA <tolga.sarikaya.dev@gmail.com>
2025-02-18 06:22:34 +0300

    Make pages dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import ImageSlider from "@/components/image-slider/image-slider";
 import MovieList from "@/components/movie-lists/movie-list";
 
+export const dynamic = "force-dynamic";
 export default async function Home() {
   return (
     <div className="flex flex-col gap-16">
--- app/upcoming/page.tsx
+++ app/upcoming/page.tsx
@@ -1,5 +1,6 @@
 import MovieGrid from "@/components/movie-grid/movie-grid";
 
+export const dynamic = "force-dynamic";
 export default function Upcoming() {
   return <MovieGrid type="upcoming" />;
 }
